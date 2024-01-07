1c95a7fb30a77406fd080fb7aa60624b1889cae8
Pnunes <pno.mco.jcn@gmail.com>
2024-01-07 15:55:38 -0300

    add key

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
             {habits !== null &&
                 Object.entries(habits).map(([habit, habitSreak]) => (
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2" key={habit}>
                         <div className="flex justify-between items-center">
                             <h1 className="font-semibold text-xl text-zinc-300">
                                 {habit}
